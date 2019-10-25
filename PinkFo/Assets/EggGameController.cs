b231a4556c1422c3db97e4f4eaf49c5f657e558d
seanred360 <41050673+seanred360@users.noreply.github.com>
2019-10-25 15:48:32 +0800

    finished and playable

--- PinkFo/Assets/EggGameController.cs
+++ PinkFo/Assets/EggGameController.cs
@@ -10,12 +10,15 @@ public class EggGameController : MonoBehaviour
     public Transform[] Eggs;
     public Transform[] EggWords;
     public Transform[] nests;
-    int wordIndex;
+    public int wordIndex;
 
     public List<GameObject> cracks;
     public GameObject crackedEgg;
     int crackNum;
     public AudioManager audioManager;
+
+    public GameObject endScreen;
+    public AudioManager endSounds;
 
     private void Start()
     {
@@ -36,8 +39,17 @@ public class EggGameController : MonoBehaviour
         EggWords[wordIndex].DOScale(19f, 1f);
         wordIndex += 1;
         yield return new WaitForSeconds(1f);
-        Eggs[wordIndex].DOMove(Vector3.zero, 1f);
-        Eggs[wordIndex].DOScale(1f, 1f);
+        if(wordIndex < Eggs.Length)
+        {
+            Eggs[wordIndex].DOMove(Vector3.zero, 1f);
+            Eggs[wordIndex].DOScale(1f, 1f);
+        }
+        if (wordIndex >= Eggs.Length)
+        {
+            endScreen.SetActive(true);
+            endSounds.PlayMusic(0);
+            endSounds.PlaySFX(Random.Range(0, 2));
+        }
     }
 
     public void CrackEgg()
@@ -55,6 +67,7 @@ public class EggGameController : MonoBehaviour
             Eggs[wordIndex].gameObject.SetActive(false);
             crackNum = 0;
             cracks.Clear();
+            if(wordIndex + 1 < Eggs.Length)
             cracks.AddRange(Eggs[wordIndex + 1].GetComponent<Egg>().cracks);
         }
         else
