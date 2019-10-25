8ec8562a9ad5116e998232a249228b5dd45fce27
seanred360 <41050673+seanred360@users.noreply.github.com>
2019-10-25 20:06:25 +0800

    bubble game moving bubbles that pop

--- PinkFo/Assets/Bubble.cs
+++ PinkFo/Assets/Bubble.cs
@@ -7,11 +7,27 @@ public class Bubble : MonoBehaviour
 {
     public Sprite[] bubbles;
     public Sprite[] words;
+    public GameObject popParticle;
     public Image wordObject;
+    AudioManager audioManager;
 
     private void Start()
     {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
         GetComponent<Image>().sprite = bubbles[Random.Range(0, bubbles.Length)];
         wordObject.sprite = words[Random.Range(0, words.Length)];
+    }
+
+    private void Update()
+    {
+        transform.position = transform.position + new Vector3(0,3 * Time.deltaTime, 0);
+    }
+
+    public void PopBubble()
+    {
+        audioManager.PlayMusic(Random.Range(0, 2));
+        audioManager.PlaySFX(0);
+        Instantiate(popParticle,transform.position,Quaternion.identity);
+        Destroy(gameObject);
     }
 }
--- PinkFo/Assets/BubbleGameController.cs
+++ PinkFo/Assets/BubbleGameController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class BubbleGameController : MonoBehaviour
 {
+    public Transform canvas;
+    public float spawnInterval;
+    public GameObject bubblePrefab;
+    public Transform[] spawnPoints;
+    Vector2 chosenSpawn;
+    Vector2 previousSpawn;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnBubble();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnBubble()
+    {
+        //for (int i = 0; i < spawnPoints.Length; i++
+        while(chosenSpawn == previousSpawn)
+        {
+            chosenSpawn = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+        }
+        
+        previousSpawn = chosenSpawn;
+        GameObject clone = Instantiate(bubblePrefab,chosenSpawn,Quaternion.identity);
+        clone.transform.SetParent(canvas);
+        clone.transform.localScale = new Vector3(2, 2, 2);
+        Destroy(clone, 8f);
+        spawnInterval = Random.Range(0, 3);
+        Invoke("SpawnBubble", spawnInterval);
     }
 }
