1a130eff8863e6a816759e3dada288a5468ec1a9
seanred360 <41050673+seanred360@users.noreply.github.com>
2019-11-01 19:39:42 +0800

    started pump game

---
+++ PinkFo/Assets/Pump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Pump : MonoBehaviour
+{
+    public Transform balloon;
+    int pumpNumber;
+    AudioManager audioManager;
+
+    private void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
+
+    public void PumpBalloon()
+    {
+        if(pumpNumber == 10)
+        {
+            Destroy(balloon.gameObject);
+            audioManager.PlaySFX(1);
+        }
+        else
+        {
+            audioManager.PlaySFX(0);
+            balloon.DOScale(balloon.localScale + new Vector3(25, 25, 25), 1f);
+            pumpNumber += 1;
+        }
+    }
+}
---
+++ PinkFo/Assets/Scripts/GumGameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class GumGameManager : MonoBehaviour
+{
+    public GameObject endScreen;
+    FoodPicker foodPicker;
+    public List<Food> m_food;
+    public AudioManager endSounds;
+    bool canPlayEndSounds = true;
+
+    bool m_isGameOver = false;
+    public bool IsGameOver { get { return m_isGameOver; } set { m_isGameOver = value; } }
+
+    private void Start()
+    {
+        m_food = (Object.FindObjectsOfType<Food>() as Food[]).ToList();
+    }
+
+    public bool AreAllFoodsEaten()
+    {
+        foreach (Food food in m_food)
+        {
+            if (!food.isEaten)
+            {
+                return false;
+            }
+        }
+        Debug.Log("all food eaten");
+        return true;
+    }
+
+    public void GameOver()
+    {
+        canPlayEndSounds = false;
+        endScreen.SetActive(true);
+        endSounds.PlayMusic(0);
+        endSounds.PlaySFX(Random.Range(0, 2));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
