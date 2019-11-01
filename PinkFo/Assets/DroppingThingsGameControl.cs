b2f9ccf6f37957f8b9d26056d393ba146f0b17dc
seanred360 <41050673+seanred360@users.noreply.github.com>
2019-11-01 19:45:58 +0800

    Dropping things game added

---
+++ PinkFo/Assets/DroppingThingsGameControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DroppingThingsGameControl : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    GameObject randomPrefab;
+    public Transform canvas;
+    GameObject clone;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            randomPrefab = prefabs[Random.Range(0, prefabs.Length)];
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            clone = Instantiate(randomPrefab, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+            clone.transform.SetParent(canvas);
+        }
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
